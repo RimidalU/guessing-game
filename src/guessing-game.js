b38b8bd85401df7dc31f721a71fdf40a81f6d280
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 <stankevichuuladimiru@gmail.com>
2021-09-03 21:55:33 +0300

    fix: task done!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
+    constructor() {
 
     }
 
-    guess() {
+    setRange(min, max) {
+this.max = max;
+this.min = min;
+    }
 
+    guess () {
+
+this.isWin = Math.round((this.max + this.min) / 2);
+
+return this.isWin;
     }
 
     lower() {
-
+ this.max = this.isWin;
     }
 
     greater() {
-
+ this.min = this.isWin;
     }
 }
 
 module.exports = GuessingGame;
+
+
